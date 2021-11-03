e3daf344bfa0172a7cdb1161da967f5e3b417453
Pratham Sonawane <78498799+DADDY-DOUBLESHOT@users.noreply.github.com>
2021-11-03 23:00:46 +0530

    Groupie added

--- ChatApp/app/src/main/java/com/example/chatapp/new_msgscreen.kt
+++ ChatApp/app/src/main/java/com/example/chatapp/new_msgscreen.kt
@@ -2,11 +2,49 @@ package com.example.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.GroupieAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+
+
+
 
 class new_msgscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_msgscreen)
         supportActionBar?.title="Select Recipient";
+
+        val adapter = GroupieAdapter();
+        adapter.add(UserItem());
+        adapter.add(UserItem());
+        adapter.add(UserItem());
+
+        findViewById<RecyclerView>(R.id.recycler_view).adapter=adapter;
+
+
+
+
+
+
+
     }
+}
+
+class UserItem() :Item<GroupieViewHolder>() {
+
+
+
+
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLayout(): Int {
+        TODO("Not yet implemented")
+        return R.layout.nmsg_view_layout;
+    }
+
 }
